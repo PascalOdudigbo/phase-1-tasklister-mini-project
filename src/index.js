75d8af05ad33fc81a0b480553182dd541baebc74
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-14 14:58:07 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById("new-task-description");
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit" ,(e) =>{
+    e.preventDefault()
+    submitForm(input.value)
+    //console.log(input.value)
+  })
 });
+
+const submitForm = (value) =>{
+  const li = document.createElement("li");
+  const ul = document.getElementById("tasks");
+  //li.textContent = value;
+  const p = document.createElement("p");
+  const btn = document.createElement("button");
+  p.textContent = `${value}  `;
+  btn.textContent = " X ";
+  btn.style.color = "red";
+  p.appendChild(btn);
+  li.appendChild(p)
+  ul.appendChild(li);
+  btn.addEventListener("click", () => btn.parentNode.parentNode.remove())
+
+}
